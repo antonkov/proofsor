d6bed88241f7b3cf2540235e844d68b4ed4e1227
Anton Kovsharov <antonkovsharov@gmail.com>
2024-10-26 17:56:11 +0200

    Some initial styling for drawing hyps

--- src/vs/editor/contrib/paperproof/browser/paperproof.contribution.ts
+++ src/vs/editor/contrib/paperproof/browser/paperproof.contribution.ts
@@ -9,13 +9,15 @@ import { Range } from '../../../common/core/range.js';
 import { ILogService } from '../../../../platform/log/common/log.js';
 import { ICodeEditor } from '../../../browser/editorBrowser.js';
 import { EditorContributionInstantiation, registerEditorContribution } from '../../../browser/editorExtensions.js';
-import { IModelDeltaDecoration, InjectedTextOptions, TrackedRangeStickiness } from '../../../common/model.js';
+import { IModelDeltaDecoration, InjectedTextCursorStops, InjectedTextOptions, TrackedRangeStickiness } from '../../../common/model.js';
 import { ILanguageFeaturesService } from '../../../common/services/languageFeatures.js';
 import { CancellationToken } from '../../../../base/common/cancellation.js';
 import { observableValue } from '../../../../base/common/observable.js';
 import { ProofTreeProvider } from '../../../common/languages.js';
 import { converter } from './converter.js';
 import { ConvertedProofTree } from './types.js';
+import { ClassNameReference, DynamicCssRules } from '../../../browser/editorDom.js';
+import { EditorOption } from '../../../common/config/editorOptions.js';
 
 interface HypChip {
 	lineNumber: number;
@@ -42,8 +44,11 @@ export class PaperproofDecorations extends Disposable implements IEditorContribu
 
 	// Decoration ids contributed by paperproof
 	private _decorationIds: string[] = [];
+
 	private readonly _sessionDisposables = new DisposableStore();
 	private readonly proofTreeProvider = observableValue<ProofTreeProvider | undefined>(this, undefined);
+	private readonly _ruleFactory = new DynamicCssRules(this.editor);
+	private _chipRule: ClassNameReference;
 
 	constructor(
 		private readonly editor: ICodeEditor,
@@ -51,6 +56,19 @@ export class PaperproofDecorations extends Disposable implements IEditorContribu
 		@ILanguageFeaturesService private readonly languageFeaturesService: ILanguageFeaturesService,
 	) {
 		super();
+
+		const editorFontSize = this.editor.getOption(EditorOption.fontSize);
+		this._chipRule = this._ruleFactory.createClassNameRef({
+			backgroundColor: '#a4dabc',
+			color: '#3a505a',
+			border: '2px solid rgb(152 214 179)',
+			height: '12px',
+			padding: '0 4px',
+			margin: '0 0 0 16px',
+			display: 'inline-block'
+		});
+
+
 
 		this._register(this.languageFeaturesService.proofTreeProvider.onDidChange(async () => {
 			this.log.info('[dbg] Proof tree changed');
@@ -110,13 +128,17 @@ export class PaperproofDecorations extends Disposable implements IEditorContribu
 		this.editor.changeDecorations(accessor => {
 			const oldDecorationIds = this._decorationIds;
 			const model = this.editor.getModel();
+
 			const decorations: IModelDeltaDecoration[] = [];
-			for (let line = 1; line <= (model?.getLineCount() || 0); line++) {
-				const lineContent = model?.getLineContent(line);
+			for (const hypChip of hypChips) {
+				if (hypChip.lineNumber === 0) {
+					continue;
+				}
 				const opts: InjectedTextOptions = {
-					content: `Line ${line}: ${lineContent?.length}`,
+					content: `${hypChip.hypothesis}`,
 					inlineClassNameAffectsLetterSpacing: true,
-					inlineClassName: 'yo',
+					inlineClassName: this._chipRule.className,
+					cursorStops: InjectedTextCursorStops.None,
 				};
 
 				const options = {
@@ -127,7 +149,7 @@ export class PaperproofDecorations extends Disposable implements IEditorContribu
 					after: opts
 				};
 
-				const range = new Range(line, 0, line, 100);
+				const range = new Range(hypChip.lineNumber, 0, hypChip.lineNumber, 100);
 				decorations.push({ range, options });
 			}
 			const newDecorationIds = accessor.deltaDecorations(this._decorationIds, decorations);
